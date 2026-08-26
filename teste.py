61bbf71f801e3c4d085be851ad2f7f2018a4cb5c
JPTirso <jptirso2@gmail.com>
2026-08-25 23:05:21 -0300

    chore: remove arquivo teste.py

--- teste.py
+++
@@ -1,8 +0,0 @@
-class Produto:
-    def __init__(self,nome,preco):
-        self.nome = nome
-        self.preco = preco        
-    def mostrar (self):
-        print(f'produto {self.nome} preco {self.preco}')
-p1 = Produto('mouse', 50)
-p1.mostrar()
